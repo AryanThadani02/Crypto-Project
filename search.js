775d260def4d72afb5ddc3a19795f6c44ffc3d9f
Aryan Thadani <aryanthadani0@gmail.com>
2024-05-09 22:29:14 +0530

    done

---
+++ search.js
@@ -0,0 +1,10 @@
+const form = document.querySelector("form")
+const input = document.querySelector("form input")
+
+form.addEventListener("submit", searchCrypto);
+
+function searchCrypto(e){
+  e.preventDefault()
+
+  
+}
